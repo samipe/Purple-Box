af59639d01c4231716d95038bc23bac4a4676acb
bob <samik@Samis-MacBook-Pro.local>
2016-10-31 09:45:21 +0200

    Revert "Merge branch 'datan-postaus'"
    This reverts commit cc323a33fd1e1067ce61b8df3813ce1953e993ee, reversing
    changes made to 6498fbc05b28226e2990cecdec5ed4a4c7bc3a5d.

--- mongoontallennus.js
+++ mongoontallennus.js
@@ -2,15 +2,14 @@ var mongoose = require('mongoose');
 var express = require("express");
 var app = express();
 var router = express.Router();
-var bodyParser = require('body-parser');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var bodyParser = require('body-parser')
 app.set('view engine', 'pug');
 
 var bussiskannaus = function(){
-    app.use( bodyParser.json() );       // to support JSON-encoded bodies ty7vcytv
-    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
+    
+app.use( bodyParser.json() );       // to support JSON-encoded bodies ty7vcytv
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
 })); 
 //app.use(express.json());
 // Connect to MongoDB and create/use database called todoAppTest
@@ -27,69 +26,74 @@ var Todo = mongoose.model('bussidataa', bussiskeema);
 
 var tiedot = mongoose.model('tiedot', bussiskeema);  
     
-var ti; // alustetaan muuttuja ti
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});       
+var ti; //alustetaan muuttuja ti
     
 app.post('/bussidata', function (req, res) {
+//  var piri = "kovaa kamaa";
     ti = new tiedot({bussiID: req.body.ID, nopeus: req.body.nopeus, yhteys: req.body.yhteys});
-    console.log('pitäisi näkyä kännyposti tässä: ', req.body)
+    console.log(req.body)
     res.status(451).send("moi");
-    // tulostettava data:
-    if(ti.bussiID === 1) {
-         io.emit('bussiID1', ('ID: ' + ti.bussiID + ', Nopeus: ' + ti.nopeus + ', Yhteys: ' + ti.yhteys + ', Viimeisin tieto: ' + ti.viimeisin_tieto));
-    } else {
-        io.emit('bussiID2', ('ID: ' + ti.bussiID + ', Nopeus: ' + ti.nopeus + ', Yhteys: ' + ti.yhteys + ', Viimeisin tieto: ' + ti.viimeisin_tieto));
-    };
+    
+    // eritellään muuttuja B
+    
+    /*var xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.send(JSON.stringify('nopeus=32'));*/
+    
+    /*var valvomoBussiID = obj.bussiID;
+    var valvomoNopeus = obj.nopeus;
+    var valvomoYhteys = obj.yhteys;*/
     
     ti.save(function(err){
   if(err)
     console.log(err);
   else
       
+    // eritellään muuttujat A 
+    /*
+    var jsonData = JSON.parse(ti);
+    for (var i = 0; i < jsonData.tiedot.length; i++) {
+        var tiedot = jsonData.tiedot[i];
+        console.log(tiedot.bussiID);
+    }
+    */
+      
     console.log("TALLENNETTU TIETOKANTAAN SEURAAVAA:  ", ti);
     return ti;    
 });
 
-
-if(ti.bussiID === 1) {
-    console.log('message1: ' + ti);
-    io.on('connection', function(socket) {    
-        socket.on('bussiID1', function(msg1){
-        });
-    });    
-} else {
-    console.log('message2: ' + ti);
-    io.on('connection', function(socket) {
-        socket.on('bussiID2', function(msg2){
-        });
-
-    });
-};        
-          
+    
+    
 router.route("/bussidata").post(function(req, res){
 var ti = ({bussiID: req.ID, nopeus: req.nopeus, yhteys: req.yhteys});
     console.log('router toimii: ', ti);
-        
+    
+    
 // Save it to database
 
+
 })
+
     
 });
+
+// RENDERÖIDÄÄN MUUTTUJAT INDEX.PUGIIN!!!
+app.get('/valvomo', function (req, res) {
+    res.render('index', { title: 'Sensoridata:', message: ti});
+    console.log("piri toimii");
+});
+// 'index' -> minne renderöidään (index.pug)
+// {}-sulkujen sisään mitä renderöidään    
     
-app.listen(3000, function () {
+app.listen(27017, function () {
         //  Start the app on the specific interface (and port).
         var ipaddress = "localhost";
-        var port = 3000;
-        console.log('kuunnellaan porttia 3000');
+        var port = 27017;
+        console.log('kuunnellaan porttia 27017');
 
     });
     
-http.listen(3001, function(){
-    console.log('listening on *;3001');
-});    
+
     
     }
 
